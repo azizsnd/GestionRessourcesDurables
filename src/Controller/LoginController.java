76a95664b807a79daba29ce3e16c14818065dfe3
Aziz Sandid <sandid.aziz123@gmail.com>
2024-12-15 12:30:40 +0100

    changes in login & session manager

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -4,6 +4,7 @@ import Model.Utilisateurs.Utilisateur;
 import Services.ServiceUser;
 import Utils.ViewLoader;
 import Utils.Alert;
+import Utils.SessionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -31,14 +32,18 @@ public class LoginController {
         Utilisateur user = ServiceUser.authenticateUser(username, password);
         
     if (user != null) {
+            SessionManager session = SessionManager.getInstance();
+            session.setCurrentUser(user);
             Stage currentStage = (Stage) loginButton.getScene().getWindow();
-            if (user instanceof Model.Utilisateurs.Administrateur) {
+            if (session.getRole()=="Responsable RSE") {
+                ViewLoader.load("../View/VueSuiviRessource.fxml", currentStage);
+            } else if (session.getRole()=="Auditeur") {
+                ViewLoader.load("../View/VueSuiviRessource.fxml", currentStage);
+            } else if (session.getRole()=="Administrateur") {
                 ViewLoader.load("../View/VueSuiviRessource.fxml", currentStage);
             } else {
-                ViewLoader.load("../View/VueSuiviRessource.fxml", currentStage);
-            }
-          } else {
                    Alert.showErrorAlert("Login Failed", "Invalid username or password.");
                }
-        }
+        } 
+    }
    }
